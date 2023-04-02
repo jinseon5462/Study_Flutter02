4e9cf4c8f1fefd9f606388b171366e895d96c6f7
jinseon5462 <dlwlstjs5462@gmail.com>
2023-04-02 23:45:02 +0900

    2023/04/02 - 일부분 수정

--- chool_check/ios/Runner/AppDelegate.swift
+++ chool_check/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBtmsMGzE0alx2z-luiRLswEkOo0qvcRjw")
+    GMSServices.provideAPIKey("AIzaSyBtmsMGzE0alx2z-luiRLswEkOo0qvcRjwqaz")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
